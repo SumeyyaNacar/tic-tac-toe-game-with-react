1c2b9240327b92880d93f8088208d5ae6ae92c28
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-12-28 14:28:14 +0300

    tic-tac-toe-game

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import React from 'react'
+import { TicTacToe } from './components/tictactoe'
+
+export const App = () => {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
